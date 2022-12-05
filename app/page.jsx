ca21903f731c50f7f976df3f92cc618e19d803e2
Lisandro939 <lisandrofernandez2705@gmail.com>
2022-12-04 21:10:31 -0300

    Change some import names that were wrong

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Countries from '../components/Countries'
-import Searcher from '../components/searcher'
+import Searcher from '../components/Searcher'
 import styles from './page.module.css'
 
 export default function page() {
